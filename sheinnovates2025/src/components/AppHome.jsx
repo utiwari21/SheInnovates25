3ee6643378499624c7f7a4a129311b1293c8e2e4
T1G3RR <tanishq.bansod@gmail.com>
2025-02-01 15:27:58 -0500

    Tanishq React

---
+++ sheinnovates2025/src/components/AppHome.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+const AppHome = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-blue-700 text-4xl font-bold">Hello, Tailwind!</h1>
+    </div>
+  );
+};
+
+export default AppHome;
